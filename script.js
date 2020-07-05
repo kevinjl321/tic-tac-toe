a2a76297db4cc84b1a2e6b5fa2451dab8611b6cd
Kevin <kevinlan321@gmail.com>
2020-07-04 23:12:24 -0700

    tentative function for switching between X and O symbol

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 //established event listener
+let ctr = 0;
+
 document.querySelectorAll('.box').forEach(item => {
     item.addEventListener('click', event =>{
-        item.innerHTML = 'X';
+        ctr++;
+        if(ctr % 2 !== 0){
+            item.innerHTML = 'X';
+        }
+        else{
+            item.innerHTML = 'O';
+        }
     });
 });
