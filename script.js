6edce8ac32fb38bc1bda1c58d4d548d80f3c8d24
Kevin <kevinlan321@gmail.com>
2020-07-05 21:14:38 -0700

    added event listeners and tentative functions for choice selections and game initiation

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-//established event listener
-let ctr = 0;
+const boxes = document.querySelectorAll('.box');
+const choiceX = document.getElementById("choice-x");
+const choiceY = document.getElementById("choice-o");
+const gameBoard = document.getElementById("game-board");
+const gameContainer = document.getElementById("game-container");
 
-document.querySelectorAll('.box').forEach(item => {
+let ctr = 0;
+let box_arr = [''];
+
+boxes.forEach(item => {
     item.addEventListener('click', event =>{
         ctr++;
         if(ctr % 2 !== 0){
             item.innerHTML = 'X';
+            box_arr.push('X')
+            item.removeEventListener('click', true);
         }
         else{
             item.innerHTML = 'O';
+            box_arr.push('O');
         }
     });
 });
+
+function activateGame(){
+    gameContainer.style.display = 'none';
+    gameBoard.style.display = 'flex';
+}
+
+choiceX.addEventListener("click", activateGame);
+choiceY.addEventListener("click", activateGame);
+
